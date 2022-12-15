a666b889fdfb413aa7334857b697be67941a4bfb
rooney <bch3454@gmail.com>
2022-12-15 19:08:12 +0900

    드림코딩 리액트: 6. CSS 라이브러리 - styled-component

--- dreamCoding/react/css-lib/src/App.js
+++ dreamCoding/react/css-lib/src/App.js
@@ -2,12 +2,32 @@ import logo from "./logo.svg";
 import "./App.css";
 import Button1 from "./components/Button1";
 import Button2 from "./components/Button2";
+import styled, { css } from "styled-components";
+
+const Continer = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  width: 150px;
+  height: 80px;
+  background: yellow;
+  ${(props) =>
+    props.primary &&
+    css`
+      background: green;
+    `};
+`;
 
 function App() {
   return (
     <>
       <Button1></Button1>
       <Button2></Button2>
+      <Continer>
+        <Button>버튼</Button>
+        <Button primary>버튼</Button>
+      </Continer>
     </>
   );
 }
