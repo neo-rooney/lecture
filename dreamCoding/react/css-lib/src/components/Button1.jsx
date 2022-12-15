ad6d2dc0bebcddc710adee2f83cb1d1b0bb64685
rooney <bch3454@gmail.com>
2022-12-15 19:01:20 +0900

    드림코딩 리액트: 6. CSS 라이브러리 - PostCSS

---
+++ dreamCoding/react/css-lib/src/components/Button1.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./Button1.module.css";
+
+const Button1 = () => {
+  return <button className={style.button}>버튼1</button>;
+};
+
+export default Button1;
---
+++ dreamCoding/react/css-lib/src/components/Button2.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./Button2.module.css";
+
+const Button2 = () => {
+  return <button className={style.button}>버튼2</button>;
+};
+
+export default Button2;
