9fc72cecb41f4328fa429541b4eea979c6fc4932
rooney <bch3454@gmail.com>
2022-12-15 18:25:04 +0900

    드림코딩 리액트: 5. 고급 내용들 - 성능 개선

--- dreamCoding/basic/src/index.js
+++ dreamCoding/basic/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AppTheme from "./AppTheme";
+import AppMontorsButton from "./AppMontorsButton";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppTheme />
+    <AppMontorsButton />
   </React.StrictMode>
 );
 
