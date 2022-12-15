e28f525b3e30a29de98c7aa5f102c9b9ab12bf6f
rooney <bch3454@gmail.com>
2022-12-15 16:53:29 +0900

    드림코딩 리액트: 5. 마우스 따라가기 - 개선 1

--- dreamCoding/basic/src/AppXY.jsx
+++ dreamCoding/basic/src/AppXY.jsx
@@ -5,24 +5,19 @@ import { useEffect, useState } from "react";
 function App() {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const changePosition = (x, y) => {
-    setX(x);
-    setY(y);
-  };
-  useEffect(() => {
-    window.addEventListener("mousemove", (e) => {
-      changePosition(e.pageX, e.pageY);
-    });
-    return () => {
-      console.log("removeEventListener");
-      window.removeEventListener("mousemove", (e) => {
-        changePosition(e.pageX, e.pageY);
-      });
-    };
-  }, []);
+
   return (
-    <div className="container">
-      <div className="pointer" style={{ top: `${y}px`, left: `${x}px` }}></div>
+    <div
+      className="container"
+      onPointerMove={(e) => {
+        setX(e.clientX);
+        setY(e.clientY);
+      }}
+    >
+      <div
+        className="pointer"
+        style={{ transform: `translate(${x}px, ${y}px)` }}
+      ></div>
     </div>
   );
 }
