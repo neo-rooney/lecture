644efa36c7d82d79dc99ce7c6a9065b644faf1ca
rooney <bch3454@gmail.com>
2022-12-19 18:52:04 +0900

    드림코딩 리액트: 유튜브 프로젝트 - 공통 / 라우팅 구조 설계

--- dreamCoding/react/youtube-clone/src/App.js
+++ dreamCoding/react/youtube-clone/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import VideoDetail from "./pages/VideoDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/videos/:keyword", element: <Search /> },
+      { path: "/videos/watch/:videoId", element: <VideoDetail /> },
+    ],
+  },
+]);
 
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
