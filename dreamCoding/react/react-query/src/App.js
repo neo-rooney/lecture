475d67152f658a94795f9cd10cd689dc7648c2f4
rooney <bch3454@gmail.com>
2022-12-19 16:57:26 +0900

    드림림림림코딩 리액트: 리액트 쿼리 (1) 기본 사용법

---
+++ dreamCoding/react/react-query/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import MainProducts from "./components/MainProducts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainProducts />
+    </QueryClientProvider>
+  );
+}
