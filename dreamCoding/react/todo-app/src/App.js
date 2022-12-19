93beeacdc319c117635a6ccb2ccec2725a3ce5c4
rooney <bch3454@gmail.com>
2022-12-19 14:59:53 +0900

    드림림림림코딩 리액트: 투두 프로젝트 챌린지 첨삭 (8) 다크모드

--- dreamCoding/react/todo-app/src/App.js
+++ dreamCoding/react/todo-app/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header/Header";
 import TodoList from "./components/TodoList/TodoList";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const filters = ["all", "active", "completed"];
 
@@ -8,10 +9,10 @@ function App() {
   const [filter, setFilter] = useState(filters[0]);
 
   return (
-    <>
+    <DarkModeProvider>
       <Header filters={filters} onChangeFilter={setFilter} filter={filter} />
       <TodoList filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 }
 
