bc506398f84991055b345c42f3fff67e81b37c87
rooney <bch3454@gmail.com>
2022-12-21 13:37:25 +0900

    드림코딩 리액트: 쇼핑몰 프로젝트 - 라우팅 구조 설계

--- dreamCoding/react/shoppy/src/App.js
+++ dreamCoding/react/shoppy/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      App
+      <Outlet />
+    </div>
+  );
 }
--- dreamCoding/react/shoppy/src/index.js
+++ dreamCoding/react/shoppy/src/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import AllProducts from "./pages/AllProducts";
+import NewProduct from "./pages/NewProduct";
+import ProductDetail from "./pages/ProductDetail";
+import MyCart from "./pages/MyCart";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "products",
+        element: <AllProducts />,
+      },
+      {
+        path: "products/new",
+        element: <NewProduct />,
+      },
+      {
+        path: "products/:id",
+        element: <ProductDetail />,
+      },
+      {
+        path: "carts",
+        element: <MyCart />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
