9da1ad4f8e9ed561370f2b5f6d34596fc4fd3d67
rooney <bch3454@gmail.com>
2022-12-26 12:10:59 +0900

    드림코딩 리액트: 쇼핑몰 프로젝트 - 장바구니 UI

--- dreamCoding/react/shoppy/src/components/CartItem.jsx
+++ dreamCoding/react/shoppy/src/components/CartItem.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { AiOutlineMinusSquare, AiOutlinePlusSquare } from "react-icons/ai";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import { addOrUpdateToCart, removeFromCart } from "../api/firebase";
+
+const ICON_CLASS =
+  "transition-all cursor-pointer hover:text-brand hover:scale-105";
 export default function CartItem({
   product,
   product: { id, image, title, options, quantity, price },
@@ -19,16 +22,19 @@ export default function CartItem({
   };
 
   return (
-    <li>
-      <img src={image} alt={title} />
-      <div>
-        <p>{title}</p>
-        <p>{options}</p>
-        <div>
-          <AiOutlineMinusSquare onClick={handleMinus} />
+    <li className="flex justify-between my-2 items-center">
+      <img className="w-24 md:w-48 rounded-lg" src={image} alt={title} />
+      <div className="flex-1 flex justify-between ml-4">
+        <div className="basis-3/5">
+          <p className="text-lg">{title}</p>
+          <p className="text-xl font-bold text-brand">{options}</p>
+          <p>₩{price}</p>
+        </div>
+        <div className="text-2xl flex items-center gap-1">
+          <AiOutlineMinusSquare onClick={handleMinus} className={ICON_CLASS} />
           <span>{quantity}</span>
-          <AiOutlinePlusSquare onClick={handlePlus} />
-          <RiDeleteBin5Fill onClick={handleDelete} />
+          <AiOutlinePlusSquare onClick={handlePlus} className={ICON_CLASS} />
+          <RiDeleteBin5Fill onClick={handleDelete} className={ICON_CLASS} />
         </div>
       </div>
     </li>
